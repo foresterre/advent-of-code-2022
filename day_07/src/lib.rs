e921eb804eb3350aa4635e988dffced81f65df6a
Martijn <garm@ilumeo.com>
2022-12-08 13:33:08 +0100

    Remove current path from FileSystem struct

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -3,14 +3,15 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct FileSystem {
-    // maps a folder, to the total size of files in the folder, we don't care about
-    // everything else for the puzzle =)
+    // Maps a folder, to the total size of files in the folder.
+    // We don't care about files themselves for the puzzle =).
     fs: HashMap<Utf8PathBuf, u32>,
-    current: Utf8PathBuf,
 }
 
 impl FileSystem {
     pub fn from_str(input: &str) -> Self {
+        let mut current = Utf8PathBuf::from("/");
+
         input.lines().fold(FileSystem::default(), |mut acc, line| {
             let (lhs, rhs) = line.split_once(' ').unwrap();
 
@@ -18,13 +19,11 @@ impl FileSystem {
                 let dir = &rhs[3..];
 
                 if dir == ".." {
-                    acc.current.pop();
+                    current.pop();
                 } else {
-                    acc.current.push(dir);
+                    current.push(dir);
                 }
             } else if let Ok(size) = lhs.parse::<u32>() {
-                let current = acc.current.as_path();
-
                 for ancestor in current.ancestors() {
                     *acc.fs.entry(ancestor.to_path_buf()).or_default() += size;
                 }
@@ -51,7 +50,6 @@ impl Default for FileSystem {
     fn default() -> Self {
         Self {
             fs: Default::default(),
-            current: Utf8PathBuf::from("/"),
         }
     }
 }
