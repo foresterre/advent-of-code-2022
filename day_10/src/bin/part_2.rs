a57a3e78247fa9855ab2a8e6dc2c3ce22ce948a6
Martijn <garm@ilumeo.com>
2022-12-17 00:01:20 +0100

    Solve day 10b

--- day_10/src/bin/part_2.rs
+++ day_10/src/bin/part_2.rs
@@ -1,5 +1,30 @@
-fn main() {
-    let input = include_str!("../../../input/day_template.txt");
+use day_10::{Instr, Machine};
 
-    println!("{}", input);
+fn main() {
+    let input = include_str!("../../../input/day_10.txt");
+
+    let observe = [20_usize, 60, 100, 140, 180, 220];
+    let mut message = String::with_capacity(256);
+
+    let mut machine = Machine::new(|cycle, reg| {
+        let x = (cycle as i32 - 1) % 40;
+        if (reg - x).abs() <= 1 {
+            message.push('#');
+            message.push('#');
+        } else {
+            message.push('.');
+            message.push('.');
+        }
+
+        if cycle % 40 == 0 {
+            message.push('\n');
+        }
+    });
+
+    input
+        .lines()
+        .map(Instr::from)
+        .for_each(|instr| machine.run_instruction(instr));
+
+    println!("{}", message);
 }
