afb5839fdc4f1a07aa9e2ea87cd5d8e12f79135b
Martijn Gribnau <garm@ilumeo.com>
2022-12-20 22:27:37 +0100

    Put parser and Expr in day 13 shared lib

--- day_13/src/bin/part_1.rs
+++ day_13/src/bin/part_1.rs
@@ -1,6 +1,6 @@
 use chumsky::prelude::*;
+use day_13::{parser, Expr};
 use std::cmp::Ordering;
-use std::slice;
 
 fn main() {
     let input = include_str!("../../../input/day_13.txt");
@@ -29,51 +29,4 @@ fn main() {
         .sum::<usize>();
 
     println!("{}", answer);
-}
-
-#[derive(Clone, Debug, Eq)]
-enum Expr {
-    Num(u8),
-    List(Vec<Self>),
-}
-
-impl Ord for Expr {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match (self, other) {
-            (Self::Num(l), Self::Num(r)) => l.cmp(r),
-            (num @ Self::Num(_l), Self::List(r)) => slice::from_ref(num).cmp(r.as_slice()),
-            (Self::List(l), num @ Self::Num(_r)) => l.as_slice().cmp(slice::from_ref(num)),
-            (Self::List(l), Self::List(r)) => l.cmp(r),
-        }
-    }
-}
-
-impl PartialOrd for Expr {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Expr {
-    fn eq(&self, other: &Self) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-
-fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let rec = recursive(|value| {
-        let int = text::int(10)
-            .map(|s: String| Expr::Num(s.parse().unwrap()))
-            .labelled("int");
-
-        let list = value
-            .separated_by(just(','))
-            .delimited_by(just('['), just(']'))
-            .map(Expr::List)
-            .labelled("list");
-
-        int.or(list)
-    });
-
-    rec.padded().then_ignore(end())
 }
--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,1 +1,53 @@
+use chumsky::error::Simple;
+use chumsky::prelude::{end, just, recursive};
+use chumsky::text::TextParser;
+use chumsky::{text, Parser};
+use std::cmp::Ordering;
+use std::slice;
 
+#[derive(Clone, Debug, Eq)]
+pub enum Expr {
+    Num(u8),
+    List(Vec<Self>),
+}
+
+impl Ord for Expr {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Self::Num(l), Self::Num(r)) => l.cmp(r),
+            (num @ Self::Num(_l), Self::List(r)) => slice::from_ref(num).cmp(r.as_slice()),
+            (Self::List(l), num @ Self::Num(_r)) => l.as_slice().cmp(slice::from_ref(num)),
+            (Self::List(l), Self::List(r)) => l.cmp(r),
+        }
+    }
+}
+
+impl PartialOrd for Expr {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Expr {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+pub fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+    let rec = recursive(|value| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .labelled("int");
+
+        let list = value
+            .separated_by(just(','))
+            .delimited_by(just('['), just(']'))
+            .map(Expr::List)
+            .labelled("list");
+
+        int.or(list)
+    });
+
+    rec.padded().then_ignore(end())
+}
